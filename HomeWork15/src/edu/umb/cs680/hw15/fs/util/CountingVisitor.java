055b195585054fc1c16d26bfaad8ecde87720771
SricharanMohanKumar <117329911+SricharanMohanKumar@users.noreply.github.com>
2022-12-19 18:00:15 -0500

    Add files via upload

---
+++ HomeWork15/src/edu/umb/cs680/hw15/fs/util/CountingVisitor.java
@@ -0,0 +1,35 @@
+package edu.umb.cs680.hw15.fs.util;
+import edu.umb.cs680.hw15.fs.*;
+import edu.umb.cs680.hw15.fs.FSVisitor;
+
+public class CountingVisitor implements FSVisitor
+{
+    private int dirNum =0;
+    private int fileNum = 0;
+    private int linkNum = 0;
+    public void visit(Link link)
+    {
+        linkNum++;
+    }
+    public void visit(Directory dir)
+    {
+        System.out.println(dirNum);
+        dirNum++;
+    }
+    public void visit(File file)
+    {
+        fileNum++;
+    }
+    public int getDirNum()
+    {
+        return dirNum;
+    }
+    public int getLinkNum()
+    {
+        return linkNum;
+    }
+    public int getFileNum()
+    {
+        return fileNum;
+    }
+}
